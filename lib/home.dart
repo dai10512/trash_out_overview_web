d0cabed3a0346637798f0f73e4360bbc6cc1db1c
Daisuke Osanai <dsk.osanai@gmail.com>
2022-06-11 07:43:47 +0900

    no message

--- lib/home.dart
+++ lib/home.dart
@@ -1,77 +1,155 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 import 'package:trash_out_overview_web/privacyPolicy%20.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(
-              maxWidth: 800,
-            ),
-            child: Column(
-              children: [
-                buildFirstMessage(context),
-                buildPrivacyPolicy(),
-              ],
+    const contactUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSfK3Atlu_Gljy2K4VuSRbNQNXN8oHR3AdywsQjqRYFs8rKOjg/viewform';
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 800,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  buildOverview(context),
+                  spacer(),
+                  buildFirstMessage(context),
+                  buildPrivacyPolicy(),
+                  spacer(),
+                  buildContactForm(context, contactUrl),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
-}
 
-Widget buildFirstMessage(context) {
-  return Column(
-    children: [
-      Text(
-        firstMessage,
-        style: Theme.of(context).textTheme.bodyLarge,
+  Widget buildOverview(context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(30)), // 調節
+
+          child: Image.asset(
+            'images/logo.png',
+            width: 160,
+            height: 160,
+          ),
+        ),
+        const SizedBox(width: 20),
+        Expanded(
+          flex: 5,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'ごみ収集日管理アプリ',
+                style: (MediaQuery.of(context).size.width > 600) ? Theme.of(context).textTheme.titleLarge : Theme.of(context).textTheme.titleSmall,
+              ),
+              Text(
+                'TrashOut',
+                style: (MediaQuery.of(context).size.width > 600) ? Theme.of(context).textTheme.displayLarge : Theme.of(context).textTheme.displayMedium,
+              ),
+              const Divider(),
+              const Text('「第2・第4水曜日」といった複雑なスケジューリングで、当日及び前日に該当するゴミの種類を通知で案内することができます'),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildFirstMessage(context) {
+    return Column(
+      children: [
+        Text(
+          firstMessage,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        spacer(),
+      ],
+    );
+  }
+
+  Widget spacer() {
+    return const SizedBox(height: 20);
+  }
+
+  Widget buildPrivacyPolicy() {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: headlineList.length,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        return policyHeadline(context, headlineList[index], contentList[index]);
+      },
+    );
+  }
+
+  Widget policyHeadline(context, title, content) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        buildTitle(context, title),
+        const Divider(),
+        buildContent(context, content),
+        spacer(),
+      ],
+    );
+  }
+
+  Widget buildContactForm(context, url) {
+    return ElevatedButton(
+      onPressed: () async {
+        if (await canLaunchUrl(Uri.parse(url))) {
+          await launchUrl((Uri.parse(url)));
+        } else {
+          throw 'Unable to launch url $url';
+        }
+      },
+      child: const SizedBox(
+        height: 60,
+        width: double.infinity,
+        child: Center(
+          child: Text('問い合わせフォーム（google Form）に移動する'),
+        ),
       ),
-      spacer(),
-    ],
-  );
-}
+    );
+  }
 
-Widget spacer() {
-  return const SizedBox(height: 20);
-}
+  Future buildLaunchUrl(url) async {
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl((Uri.parse(url)));
+    } else {
+      throw 'Unable to launch url $url';
+    }
+  }
 
-Widget buildPrivacyPolicy() {
-  return ListView.builder(
-    shrinkWrap: true,
-    itemCount: headlineList.length,
-    physics: const NeverScrollableScrollPhysics(),
-    itemBuilder: (BuildContext context, int index) {
-      return policyHeadline(context, headlineList[index], contentList[index]);
-    },
-  );
-}
+  Widget buildTitle(context, title) {
+    return Text(
+      title,
+      style: (MediaQuery.of(context).size.width > 600) ? Theme.of(context).textTheme.titleLarge : Theme.of(context).textTheme.titleMedium,
+    );
+  }
 
-Widget policyHeadline(context, title, content) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      buildTitle(context, title),
-      const Divider(),
-      buildContent(context, content),
-      spacer(),
-    ],
-  );
-}
-
-Widget buildTitle(context, title) {
-  return Text(title, style: Theme.of(context).textTheme.headlineMedium);
-}
-
-Widget buildContent(context, content) {
-  return Text(content, style: Theme.of(context).textTheme.bodyLarge);
+  Widget buildContent(context, content) {
+    return Text(
+      content,
+      style: Theme.of(context).textTheme.bodyLarge,
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,12 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'NotoSansJP',
         useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
